0d623b9f93611fae36e14b93ed6d02477f604e32
owari-taro <58625526+owari-taro@users.noreply.github.com>
2023-12-05 13:21:30 +0900

    app

--- celery_trial/celery_with_django/REAEMD.md
+++ celery_trial/celery_with_django/REAEMD.md
@@ -10,7 +10,8 @@ celery -A CELERY worker --loglevel=INFO
 ```
 ### run worker for specific queues
 ```
-celery worker -A scheduler -l info -Q team_actions
+
+celery -A config worker --loglevel=INFO -c 1 -Q long,short
 #複数ワーカーを動かしたいとき↓
 celery multi start -A config node1 node2 -c:1 1 -Q:1 long -c:2 5 -Q:2 short
 
--- celery_trial/celery_with_django/app/tasks.py
+++ celery_trial/celery_with_django/app/tasks.py
@@ -1,17 +1,22 @@
 from celery import shared_task
 import time
+from celery.exceptions import SoftTimeLimitExceeded
 
-@shared_task
-def long_task(seconds:int,short):
-    print("please wait a minutes",seconds,sep="/n")
-    #pr\int("asdlfjaslkfjaslfj\n\n")
+
+# max retryを超えたときはsoft_time_limit exceptionがなげられる
+@shared_task(
+    max_retries=3, soft_time_limit=3, autoretry_for=(Exception,), retry_backoff=True
+)
+def long_task(seconds: int):
+    print("please wait a minutes", seconds, sep="/n")
+    # pr\int("asdlfjaslkfjaslfj\n\n")
     time.sleep(seconds)
     print("finished!!!!!!!!!\n")
 
 
 @shared_task(soft_time_limit=10)
-def short_task(seconds:int):
-    print("please wait a minutes",seconds,sep="/n")
-    #pr\int("asdlfjaslkfjaslfj\n\n")
+def short_task(seconds: int):
+    print("please wait a minutes", seconds, sep="/n")
+    # pr\int("asdlfjaslkfjaslfj\n\n")
     time.sleep(0.5)
     print("finished!!!!!!!!!\n")
